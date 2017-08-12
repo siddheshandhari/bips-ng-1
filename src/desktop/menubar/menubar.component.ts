593883eb20feb159ef9f86aae84db193a788d5c6
Yanhao Li <hi@yanhaoli.com>
2017-08-12 12:30:42 -0400

    add menubar animation

--- src/desktop/menubar/menubar.component.ts
+++ src/desktop/menubar/menubar.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'menubar',
   templateUrl: 'menubar.component.html',
-  styleUrls: ['menubar.component.css']
+  styleUrls: ['menubar.component.css'],
+  animations: [
+    trigger('menuDisplayState', [
+      state('hide', style({
+        bottom: "-70px",
+        opacity: 0.25
+      })),
+      state('show', style({
+        bottom: "0",
+        opacity: 1
+      })),
+      transition('hide => show', animate('500ms ease-out')),
+      transition('show => hide', animate('500ms 3s ease-in'))
+    ])
+  ]
 })
-export class MenubarComponent {
+export class MenubarComponent implements OnInit {
+  menuDisplay: string;
 
+  constructor(){
+    this.menuDisplay = 'hide';
+  }
+
+  ngOnInit(){
+
+  }
+
+  hideMenu(){
+    this.menuDisplay = 'hide';
+  }
+
+  showMenu(){
+    this.menuDisplay = 'show';
+  }
 }
