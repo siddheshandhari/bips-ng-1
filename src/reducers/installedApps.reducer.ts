d794b3b8f80bd241a904f4c013dc0db99ed367b4
Yanhao Li <hi@yanhaoli.com>
2017-08-22 16:41:57 -0400

    fixed sidebar bug

--- src/reducers/installedApps.reducer.ts
+++ src/reducers/installedApps.reducer.ts
@@ -1,4 +1,4 @@
-const preInstalledApps = [22];
+const preInstalledApps = [22, 9, 11];
 
 export const ADD_INSTALLED_APPS = 'ADD_INSTALLED_APPS';
 export const REMOVE_INSTALLED_APPS = 'REMOVE_INSTALLED_APPS';
