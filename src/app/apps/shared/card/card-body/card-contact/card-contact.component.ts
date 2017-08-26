33aa560e0b58cb1c5a64834a3a16f86ce3c36e9b
YaliW <yw1976@nyu.edu>
2017-08-26 17:24:52 -0400

    card-contact

---
+++ src/app/apps/shared/card/card-body/card-contact/card-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'card-contact',
+  templateUrl: 'card-contact.component.html',
+  styleUrls: ['card-contact.component.css']
+})
+export class CardContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
