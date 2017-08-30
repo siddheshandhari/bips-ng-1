4275c06381bde0ed2b2b31fc7ddd1c80ff5f4fdd
YaliW <yw1976@nyu.edu>
2017-08-30 15:37:33 -0400

    card-multiple contacts

--- src/app/apps/shared/card/card-body/card-contact/card-contact.component.ts
+++ src/app/apps/shared/card/card-body/card-contact/card-contact.component.ts
@@ -4,12 +4,11 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'card-contact',
   templateUrl: 'card-contact.component.html',
   styleUrls: ['card-contact.component.css'],
-  inputs: ['./card-body'],
-
+  
 })
 export class CardContactComponent implements OnInit {
-  @Input() name: string;
-  @Input() position: string;
+  
+  @Input() contact: object;
 
   constructor() { }
 
