10d4ae20baf6445fbe47428b327c053bc8d7ee8d
Yanhao Li <hi@yanhaoli.com>
2017-09-02 09:26:54 -0400

    add flex layout onto accounts list

--- src/app/apps/account/account-list/account-list.component.ts
+++ src/app/apps/account/account-list/account-list.component.ts
@@ -10,19 +10,28 @@ import { Account } from '../account';
 
 export class AccountListComponent implements OnInit {
   public accountList: Array<Account> = [];
+  public accountListCol1: Array<Account> = [];
+  public accountListCol2: Array<Account> = [];
+  public accountListCol3: Array<Account> = [];
+  public itemsPerCol: number;
   constructor( public accountService: AccountService){}
 
   ngOnInit(){
     this.accountService.getAccountList().subscribe(
       res => {
         this.accountList = res;
-        console.log(this.accountList);
+        this.itemsPerCol = Math.floor(this.accountList.length / 3);
+        this.accountListCol1 = this.accountList.slice(0, this.itemsPerCol);
+        this.accountListCol2 = this.accountList.slice(this.itemsPerCol, this.itemsPerCol * 2);
+        this.accountListCol3 = this.accountList.slice(this.itemsPerCol * 2);
       },
       error => {
         console.log(error)
       }
     );
   }
+
+
 
 
 }
