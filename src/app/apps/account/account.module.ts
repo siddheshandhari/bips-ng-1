fff16a7251dd81497600ae37ddeba0e2c173b546
Yanhao Li <hi@yanhaoli.com>
2017-09-02 08:56:28 -0400

    add waterfall component

--- src/app/apps/account/account.module.ts
+++ src/app/apps/account/account.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AccountComponent } from './account.component';
-import { AccountListComponent } from './account-list/account-list.component';
-import { AccountService } from './account.service';
 import { CardModule } from '../shared/card/card.module';
 import { AddButtonModule } from '../shared/add-button/add-button.module';
+
+import { AccountComponent } from './account.component';
+import { AccountListComponent } from './account-list/account-list.component';
+
+import { AccountService } from './account.service';
+
 
 @NgModule({
   declarations: [
     AccountComponent,
-    AccountListComponent
+    AccountListComponent,
   ],
   imports: [
     CardModule,
     AddButtonModule,
-    CommonModule
+    CommonModule,
   ],
   exports: [
     AccountComponent,
