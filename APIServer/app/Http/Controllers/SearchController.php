d7606a2b7d096a96b45494e0b079034cfd2e6315
Yanhao Li <hi@yanhaoli.com>
2017-09-05 11:47:07 -0400

    add search controller

--- APIServer/app/Http/Controllers/SearchController.php
+++ APIServer/app/Http/Controllers/SearchController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Lead;
 use Illuminate\Http\Request;
 
 class SearchController extends ApiController
 {
-  public function search()
+  public function search(Request $request)
   {
-    
+    if($request->has('category') && $request->has('context') && $request->has('value')) {
+      $category = $request->input('category');
+      $context = $request->input('context');
+      $value = $request->input('value');
+    } else {
+      return $this->setStatusCode(500)->respondWithError("Query keys not completed");
+    }
+
+    if($category == "lead"){
+      
+    } else {
+      return $this->setStatusCode(500)->respondWithError("Doesn't support this category!");
+    }
   }
 }
