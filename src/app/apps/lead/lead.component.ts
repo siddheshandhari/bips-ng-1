6ec8fd5a1c67f6f918cf6dc66510ab4e61814b87
Yanhao Li <hi@yanhaoli.com>
2017-09-07 11:15:14 -0400

    fix lead list scroll problem

--- src/app/apps/lead/lead.component.ts
+++ src/app/apps/lead/lead.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, Output  } from '@angular/core';
-
-import { LeadService } from './lead.service';
-import { Lead } from './lead';
-
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'lead',
@@ -11,70 +7,14 @@ import { Lead } from './lead';
 
 })
 
-export class LeadComponent implements OnInit {
+export class LeadComponent {
+  modalOpen: boolean = true;
 
-    leadlist : Lead[] = [];
-    selectedLead : Lead;
-    add_lead_value = false;
-    modalOpen: boolean = true;
-
-    constructor(public leadService:LeadService){}
-
-// get lead infomation
-    getLeadlist():void{
-          this.leadService.getLeadlist()
-          .subscribe(leadlist => this.leadlist = leadlist)
-    }
-
-// delete lead
-    deleteLead(lead:Lead): void{
-
-        this.leadService.deleteLead(lead.id)
-         .subscribe(()=>{
-             this.leadlist = this.leadlist.filter (l => l !== lead);
-             if(this.selectedLead === lead){this.selectedLead = null}
-         })
-
-   }
-
-    deleteCard(id: number){
-    this.leadService.deleteLead(id);
-    this.leadlist = this.leadlist.filter(lead => {
-      return lead.id !== id;
-    })
+  openModal(){
+    this.modalOpen = true;
   }
 
-    ngOnInit():void{
-        this.getLeadlist();
-    }
-
-
-
-    editLead(lead:Lead): void{
-
-            this.selectedLead = lead;
-            console.log(this.selectedLead);
-    }
-
-// jump to add lead page
-
-// go back to lead page
-    cancel_lead(){
-
-        this.getLeadlist()
-    }
-// submit form
-    save_lead(){
-        // this.add_lead_value = false;
-         this.getLeadlist()
-    }
-
-    openModal(){
-      this.modalOpen = true;
-    }
-
-    closeModal(){
-      this.modalOpen = false;
-    }
-
+  closeModal(){
+    this.modalOpen = false;
+  }
 }
