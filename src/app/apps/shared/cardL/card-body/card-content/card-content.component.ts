217dfc33129c18d62e0ff906a763cea6f5af2198
simon <simon@orcasmart.com>
2017-09-09 16:59:24 -0400

    save

---
+++ src/app/apps/shared/cardL/card-body/card-content/card-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'card-content',
+    templateUrl: 'card-content.component.html',
+    styleUrls: ['card-content.component.css']
+})
+
+export class CardContentComponent{
+    
+}
--- src/reducers/runningApps.reducer.ts
+++ src/reducers/runningApps.reducer.ts
@@ -2,7 +2,7 @@ export const ADD_RUNNING_APPS = 'ADDED_RUNNING_APP';
 export const REMOVE_RUNNING_APPS = 'REMOVE_RUNNING_APP';
 
 //Lead = 12; Account = 1
-const initialState = [12];
+const initialState = [15];
 
 export function runningAppsReducer(state = initialState, action){
   switch (action.type) {
