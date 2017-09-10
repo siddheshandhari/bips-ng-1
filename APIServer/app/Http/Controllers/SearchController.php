5daad41a549c96728abff5903435cbc99fa28448
Yanhao Li <hi@yanhaoli.com>
2017-09-10 08:07:00 -0400

    add search api comment

--- APIServer/app/Http/Controllers/SearchController.php
+++ APIServer/app/Http/Controllers/SearchController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Company;
+use App\Lead;
 use App\utils\Builder\CompanyBuilder;
 use Illuminate\Http\Request;
 
 class SearchController extends ApiController
 {
+  // http:// localhost/api/v1/category=lead&context=name&value=xxx
   public function search(Request $request)
   {
     if($request->has('category') && $request->has('context') && $request->has('value')) {
@@ -15,10 +16,10 @@ class SearchController extends ApiController
       $context = $request->input('context');
       $value = $request->input('value');
     } else {
-      return $this->setStatusCode(400)->respondWithError("Query keys haven't completed");
+      return $this->setStatusCode(400)->respondWithError("category or context or value not specified!");
     }
 
-    if($category == "company"){
+    if($category == "lead"){
       if($context == "name"){
         $companies = Company::where('name', 'like', '%'.$value.'%')->get();
         return $this->respond($companies);
