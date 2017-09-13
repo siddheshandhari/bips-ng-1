521c51a5733fb26a409d2ebc7ba90e66cd884b18
sid <siddhesh.andhari@orcasmart.com>
2017-09-13 13:36:45 -0400

    prices get api working

--- APIServer/database/seeds/ProductsTableSeeder.php
+++ APIServer/database/seeds/ProductsTableSeeder.php
@@ -14,19 +14,15 @@ class ProductsTableSeeder extends Seeder
         $faker = Faker\Factory::create();
         foreach(range(1, 30) as $index)
         {
-          DB::table('users')->insert([
-            'email' => $faker->companyEmail,
-            'password' => $faker->password,
-            'first_name' => $faker->firstName,
-            'last_name' => $faker->lastName,
-            'nick_name' => $faker->name,
-            'gender' => $faker->randomElement($array = array('male', 'female')),
-            'ssn' => $faker->ssn,
-            'country' => $faker->country,
-            'time_zone' => $faker->timezone,
-            'phone' => $faker->phoneNumber,
-            'level' => $faker->randomDigit,
-            'group_id' => $faker->randomDigit,
+          DB::table('products')->insert([
+            'name' => $faker->name,
+            'image' => $faker->url,
+            'sector' => $faker->name,
+            'shape' => $faker->name,
+            'model' => $faker->name,
+            'details' => $faker->word,
+            'technical_detail_link' => $faker->url,
+            'description' => $faker->name,
           ]);
         }
     }
