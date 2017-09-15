3e7e5cd549815435236750571f4fa533ce4ee705
Yanhao Li <hi@yanhaoli.com>
2017-09-15 13:56:12 -0400

    import company transformer into searchController

--- APIServer/app/Http/Controllers/SearchController.php
+++ APIServer/app/Http/Controllers/SearchController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Company;
 use App\Lead;
-use Illuminate\Support\Facades\Schema;
-use App\utils\Builder\CompanyBuilder;
+
+use App\utils\Transformers\CompanyTransformer;
 use Illuminate\Http\Request;
 
 class SearchController extends ApiController
 {
+  private $query = '';
+  private $filter = '';
+  private $with = '';
+  private $company_filters = ["name", "website", "industry"];
+  private $contact_filters = ["first_name", "last_name", "email", "title", "position", "phone", "department", "home_phone", "other_phone", "fax", "mobile", "birthday", "assistant", "asst_phone", "reports_to", "skype", "customer_type", "twitter"];
+  private $lead_filters = ["source", "status", "active"];
+  private $address_filters = ["country", ""];
+
+  protected $companyTransformer;
+
+  function __construct(CompanyTransformer $companyTransformer)
+  {
+    $this->companyTransformer = $companyTransformer;
+  }
   // http:// localhost/api/v1/search/{category}?f=&q=
   //q is required.
   public function search(Request $request, $category) {
@@ -18,19 +32,23 @@ class SearchController extends ApiController
       return $this->setStatusCode(400)->respondWithError("Must specified query!");
     };
 
-    $query = $request->input('q');
-    $filter = '';
+    $this->query = $request->input('q');
+    $this->filter = '';
+    $this->with = '';
     //if filter exist, apply the filter
     if($request->has('f')){
-      $filter = $request->input('f');
+      $this->filter = $request->input('f');
+    }
+    if($request->has('w')){
+      $this->with = $request->input('w');
     }
 
     switch ($category){
       case 'company':
-        return $this->searchCompany($filter, $query);
+        return $this->searchCompany();
         break;
       case 'lead':
-        return $this->searchLead($filter, $query);
+        return $this->searchLead();
         break;
       case 'account':
         return $this->setStatusCode(400)->respondWithError("This category does not support!");
@@ -44,19 +62,36 @@ class SearchController extends ApiController
     }
   }
 
-  public function searchCompany($filter, $query){
-    //if filter does not available, throw the error
-    if(!in_array($filter, ["", "name", "website", "industry"])){
-      return $this->setStatusCode(400)->respondWithError("Does not support this filter: ".$filter);
-    };
+  private function searchCompany(){
     //if filter has not been set, use default value "company_name"
-    if(!$filter){
-      $filter = "name";
+    if(!$this->filter){
+      $this->filter = "name";
     };
-    return Company::where($filter, 'like', '%'.$query.'%')->get();
+    //if with not set, use company fields directly
+    if(!$this->with){
+      //if filter does not available, throw the error
+      if(!in_array($this->filter, $this->company_filters)){
+        return $this->setStatusCode(400)->respondWithError("Company search does not support this filter: ".$this->filter);
+      };
+      $companies = Company::where($this->filter, 'like', '%'.$this->query.'%')->get();
+      return $this->setStatusCode(200)->respond(
+        $this->companyTransformer->transformCollection($companies)
+      );
+    };
+
+    if($this->with == "contact"){
+      return Company::with(['contacts' => function($q){
+        $q->where($this->filter, 'like', '%'.$this->query.'%');
+      }])->get();
+    } else if($with == "address"){
+
+    } else {
+      return $this->setStatusCode(400)->respondWithError("Does not support search company with ".$this->with);
+    }
+
   }
 
-  public function searchLead($filter, $query){
+  private function searchLead($filter, $query){
 
   }
 
