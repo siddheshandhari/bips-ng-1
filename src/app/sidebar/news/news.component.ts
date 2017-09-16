65e6fb6f331dca68d1fed9c8908a70238e2701d0
Yanhao Li <hi@yanhaoli.com>
2017-09-16 15:44:38 -0400

    add alert news dashboard to sidebar

---
+++ src/app/sidebar/news/news.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'news',
+  templateUrl: 'news.component.html',
+  styleUrls: ['news.component.css']
+})
+
+export class NewsComponent{}
--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -7,9 +7,13 @@ import { IconModule } from '../icon/icon.module';
 //Component
 import { SidebarComponent } from './sidebar.component';
 import { ToolsComponent } from './tools/tools.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AlertComponent } from './alert/alert.component';
+import { NewsComponent } from './news/news.component';
 import { SnackbarComponent } from './snackbar/snackbar.component';
 import { SnackbarItemComponent } from './snackbar/snackbar-item/snackbar-item.component';
 import { UserComponent } from './user/user.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 @NgModule({
   imports: [
@@ -21,7 +25,11 @@ import { UserComponent } from './user/user.component';
     SnackbarComponent,
     ToolsComponent,
     SnackbarItemComponent,
-    UserComponent
+    UserComponent,
+    SectionHeaderComponent,
+    DashboardComponent,
+    AlertComponent,
+    NewsComponent
   ],
   exports: [
     SidebarComponent
