672f47ef0f83fe996e4de4611688d2ac05fe33a3
Yanhao Li <hi@yanhaoli.com>
2017-09-16 11:55:42 -0400

    set flex to statusbar

--- src/app/statusbar/statusbar.component.ts
+++ src/app/statusbar/statusbar.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'statusbar',
-  templateUrl: './statusbar.component.html',
-  styleUrls: ['./statusbar.component.css']
+  templateUrl: 'statusbar.component.html',
+  styleUrls: ['statusbar.component.css']
 })
 
 export class StatusbarComponent {
-  
+
 }
