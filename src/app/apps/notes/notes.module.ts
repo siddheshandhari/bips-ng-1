dc3b42f114cc5db5e2d273b946d36ad39e85cb65
Yanhao Li <hi@yanhaoli.com>
2017-09-17 09:48:30 -0400

    added add button on notes

--- src/app/apps/notes/notes.module.ts
+++ src/app/apps/notes/notes.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AddButtonModule } from '../shared/add-button/add-button.module';
+import { ModalModule } from '../shared/modal/modal.module';
 import { NotesComponent } from './notes.component';
 
 @NgModule({
+  imports: [
+    AddButtonModule,
+    ModalModule,
+    CommonModule
+  ],
   declarations: [
     NotesComponent
-  ],
-  providers: [
-
   ],
   exports: [
     NotesComponent
