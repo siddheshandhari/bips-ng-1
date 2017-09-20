47c2981e217c0a9904558889f3d516868a4c43ba
Yanhao Li <hi@yanhaoli.com>
2017-09-20 12:16:31 -0400

    display the first note as selected note

--- src/app/apps/notes/notes.service.ts
+++ src/app/apps/notes/notes.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 import { Note } from '../../models/note';
 
 @Injectable()
 export class NotesService{
-  public selectedNote: Note;
-  public setSelectedNote(note: Note){
-    this.selectedNote = note;
+  private selectedNote = new Subject<Note>();
+
+  selectedNote$ = this.selectedNote.asObservable();
+
+  setSelectedNote(note: Note){
+    this.selectedNote.next(note)
   }
 }
