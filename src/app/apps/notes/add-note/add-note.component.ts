b0294b9ff909de05dafe6e2b4d3b87ec98d168e3
Yanhao Li <hi@yanhaoli.com>
2017-09-22 16:17:21 -0400

    add note component

--- src/app/apps/notes/add-note/add-note.component.ts
+++ src/app/apps/notes/add-note/add-note.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CompanyService } from '../../../core/services/company.service';
+import { NotesService } from '../notes.service';
 import { Company } from '../../../core/models/index';
 
 @Component({
@@ -12,12 +13,14 @@ import { Company } from '../../../core/models/index';
 export class AddNoteComponent implements OnInit{
 
   companies: Array<Company>;
+  contextLabel: string;
   contexts: Array<any> =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
   contextVisiable: boolean = false;
   noteForm: FormGroup;
   categories: Array<string> = ['Sales Order', 'Quotes', 'Invoice', 'Projects'];
+  @Output() requestCloseModal = new EventEmitter();
 
-  constructor(private companyService: CompanyService, private fb: FormBuilder) {
+  constructor(private companyService: CompanyService, private notesService: NotesService, private fb: FormBuilder) {
     this.createForm();
   }
 
@@ -60,14 +63,17 @@ export class AddNoteComponent implements OnInit{
     const category = this.noteForm.get('category').value;
     const companyId = this.noteForm.get('company').value;
     if(category == "projects"){
+      this.contextLabel = "Project number";
         // TODO: search api for project
     } else if(category == "Quotes"){
+      this.contextLabel = "Quote number";
         // TODO: search api for quotes
     } else if(category == "Invoice"){
+      this.contextLabel = "Invoice number";
         // TODO: search api for invoice
     } else {
+      this.contextLabel = "Sales Order number";
       //category == sales order
-      console.log('hi');
     }
   }
 
@@ -92,7 +98,12 @@ export class AddNoteComponent implements OnInit{
 
 
   onSave(){
-    console.log(this.noteForm.value);
+    const note = this.noteForm.value;
+    this.notesService.addNote(note).subscribe(
+      res => {
+        this.requestCloseModal.emit();
+      }
+    );
   }
 
 
--- src/app/apps/notes/notes.service.ts
+++ src/app/apps/notes/notes.service.ts
@@ -1,26 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { Subject } from 'rxjs/Subject';
 import { Note } from '../../core/models/index';
+import { AppConfig, APP_CONFIG } from '../../app.config';
 
 @Injectable()
 export class NotesService{
-  constructor(public http: Http) {}
+  constructor(@Inject(APP_CONFIG) private config: AppConfig, public http: Http) {}
 
   getNoteList(): Observable<Note[]>{
-    return this.http.get('http://127.0.0.1:8000/api/v1/note')
+    return this.http.get(this.config.apiEndPoint + 'note')
     .map((res:Response) => {
       return res.json();
     })
     .catch((error: any) => Observable.throw(error || 'Server error'));
   }
 
-  deleteNote(id){
-    return this.http.delete('http://127.0.0.1:8000/api/v1/note/' + id)
+  deleteNote(id): Observable<any> {
+    return this.http.delete(this.config.apiEndPoint + 'note/' + id)
     .map((res:Response) => {
       return res.json();
     })
+  }
+
+  addNote(note): Observable<any> {
+    return this.http.post(this.config.apiEndPoint + 'note', JSON.stringify(note));
   }
 
 }
