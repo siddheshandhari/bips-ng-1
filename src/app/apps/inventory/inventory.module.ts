12f435a946bf7eb4a31960a970e6cf2c4ea9b799
YaliW <yw1976@nyu.edu>
2017-09-28 11:54:37 -0400

    add inventory navbar

--- src/app/apps/inventory/inventory.module.ts
+++ src/app/apps/inventory/inventory.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { InventoryComponent } from './inventory.component';
+import { InventoryListComponent } from './inventory-list/inventory-list.component';
 
 
 @NgModule({
   declarations: [
     InventoryComponent,
+    InventoryListComponent,
   ],
   exports: [
     InventoryComponent
