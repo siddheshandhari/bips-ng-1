a3719b679970247f66048fb5eec3de7a89fbf3d5
Yanhao Li <hi@yanhaoli.com>
2017-09-28 18:02:07 -0400

    signup validation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SignupService } from './signup.service';
 
 @Component({
@@ -8,19 +8,76 @@ import { SignupService } from './signup.service';
   styleUrls: ['signup.component.css']
 })
 
-export class SignupComponent {
-
+export class SignupComponent implements OnInit{
   signupForm: FormGroup;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(100)]);
+  passwordConfirm = new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(100)]);
 
-  constructor(private signupService: SignupService, private fb: FormBuilder){
+  validationMessages = {
+    'email': {
+      'required': 'This field is required',
+      'email': 'Email address must be valid'
+    },
+    'password': {
+      'required': 'This field is required',
+      'minlength': 'Password must contains at least 6 characters',
+      'maxlength': 'Password must contains at most 100 characters'
+    },
+    'passwordConfirm': {
+      'required': 'This field is required',
+      'minlength': 'Password must contains at least 6 characters',
+      'maxlength': 'Password must contains at most 100 characters'
+    },
+  };
+
+  constructor(private signupService: SignupService, private fb: FormBuilder){}
+
+  ngOnInit(){
     this.createForm();
   }
 
-  createForm(){
+  createForm(): void {
     this.signupForm = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      passwordConfirm: ['', Validators.required]
+      email: this.email,
+      password: this.password,
+      passwordConfirm: this.passwordConfirm
+    },
+    //cross form validation
+    {validator: this.matchingPasswords('password', 'passwordConfirm')}
+    );
+    this.signupForm.valueChanges.subscribe(
+      form => this.onFormChange(form)
+    );
+  }
+
+  onFormChange(form?: any){
+    if (!this.signupForm){
+      return;
+    }
+    this.checkFormError();
+  }
+
+  checkFormError(){
+    //if the form is valid, do nothing
+    if (this.signupForm.valid){
+      return;
+    }
+    //else get all form controls in the form
+    const keys = Object.keys(this.signupForm.value);
+    keys.forEach(key => {
+      const control = this.signupForm.controls[key];
+      //if control was touched and has errors
+      if(control.errors){
+        const messages = this.validationMessages[key];
+        if('required' in control.errors){
+          control.setErrors({'message': messages['required']});
+        } else {
+          for(const error in control.errors){
+            control.setErrors({'message': messages[error]});
+          }
+        }
+      }
     });
   }
 
@@ -28,8 +85,15 @@ export class SignupComponent {
     console.log(this.signupForm.valid);
   }
 
-  isValid(control){
-    return this.signupForm.controls[control].valid;
+  matchingPasswords(passwordKey: string, passwordConfirmKey: string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      const password = group.controls[passwordKey];
+      const passwordConfirm = group.controls[passwordConfirmKey];
+      if(password.value !== passwordConfirm.value) {
+        return {
+          mismatchedPassword: true
+        };
+      }
+    }
   }
-
 }
